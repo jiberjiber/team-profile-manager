332da5f61debab1ce6030c6bf46ec8a98b4de005
Evan Boswood <evan@boswood.net>
2020-07-03 17:02:22 -0500

    fixed prompts showing incorrectly

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 const { createSecureServer } = require("http2");
+const { async } = require("rxjs/internal/scheduler/async");
 
 // Variable creation ==========================================================
 let userList = [];
@@ -18,11 +19,6 @@ const managerQuestions = [
         type: "input",
         name: "name",
         message: "Manager's name: "
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Manager's id: "
     },
     {
         type: "input",
@@ -44,17 +40,12 @@ const engineerQuestions = [
     },
     {
         type: "input",
-        name: "id",
-        message: "Engineer's id: "
-    },
-    {
-        type: "input",
         name: "email",
         message: "Engineer's email: "
     },
     {
         type: "input",
-        name: "office",
+        name: "github",
         message: "Engineer's github username: "
     }
 ];
@@ -67,17 +58,12 @@ const internQuestions = [
     },
     {
         type: "input",
-        name: "id",
-        message: "Intern's id: "
-    },
-    {
-        type: "input",
         name: "email",
         message: "Intern's email: "
     },
     {
         type: "input",
-        name: "office",
+        name: "school",
         message: "Intern's school: "
     }
 ];
@@ -145,8 +131,6 @@ function createUser(){
                 createIntern();
                 break;
         }
-
-        mainMenu();
     });
 }
 
@@ -157,30 +141,76 @@ function createManager(){
 
         userList.push(newUser);
         console.log(`Manager ${answers.name} created!`);
+        mainMenu();
     });
 }
 
 function creatEngineer(){
     inquirer.prompt(engineerQuestions)
     .then(answers => {
-        var newUser = new Engineer(answers.name, userList.length + 1, answers.email, answers.github)
+        var newUser = new Engineer(answers.name, userList.length + 1, answers.email, answers.github);
+
+        userList.push(newUser);
+        console.log(`Engineer ${answers.name} created!`);
+        mainMenu();
     });
 }
 
 function createIntern(){
     inquirer.prompt(internQuestions)
     .then(answers => {
-        
+        var newUser = new Intern(answers.name, userList.length + 1, answers.email, answers.school);
+
+        userList.push(newUser);
+        console.log(`Intern ${answers.name} created!`);
+        mainMenu();
     });
 }
 
 function generateHTML(){
-    render(userList);
+    console.log(userList);
+    mainMenu();
 }
 
 function deleteUser(){
+    if(userList.length < 1){
+        console.log("No users exist!");
+        return;
+    }
+    let tempList = [];
+    for(let i = 0; i < userList.length; i++){
+        let tempName = userList[i].getName;
+        console.log(tempName);
+        tempList.push(tempName);
+    }
 
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "name",
+            message: "Choose a user to delete: ",
+            choices: tempList
+        }
+    ])
+    .then(answers => {
+        for(let i = 0; i < userList.length; i++){
+            if(userList[i].name === answers.name){
+                userList.splice(i,1);
+            }
+        }
+
+        console.log(`User ${answers.name} removed!`);
+        mainMenu();
+    });
+
+    
 }
+
+
+
+// Call main menu ===================================================================
+mainMenu();
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
